8d3774d11510a6f142e39fb5d1bfe6938dc5cf7f
fahimhasandev <fhasan5@wgu.edu>
2025-11-08 15:15:17 -0600

    Loading Seed Data

---
+++ db/seed.ts
@@ -0,0 +1,22 @@
+
+
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import sampleData from "./sample-data";
+
+
+async function main() {
+    const prisma = new PrismaClient();
+
+    //Delete all products -- prisma.(model name).deletemany
+    await prisma.product.deleteMany();
+
+    await prisma.product.createMany({
+        data: sampleData.products
+    });
+
+    console.log('Database seeded successfully');
+}
+
+main();
+
+
